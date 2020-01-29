12af740e8b209720dc1a293d96ab7dafa80bd313
Moises Huerta <mhuerta@outlook.com>
2020-01-29 12:39:41 -0800

    adding model

---
+++ GitHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GitHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet <Gig> DbSet { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
